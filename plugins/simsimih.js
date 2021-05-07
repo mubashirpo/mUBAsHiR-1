c5f29150d361a2fa6cf62152878a2482d0c21b3c
T0X!C D3V!L <jinnherefan2021@gmail.com>
2021-05-07 19:33:39 +0530

    📢 WhatsAsenaPublic Version 1.2 Is Available Now!

--- plugins/simsimih.js
+++ plugins/simsimih.js
@@ -1,7 +1,11 @@
-/* Copyright (C) 2021 Vai838.
+/* Copyright (C) 2020 TOXIC DEVIL
+
+CODDED BY TOXIC DEVIL
+
 Licensed under the  GPL-3.0 License;
+
 you may not use this file except in compliance with the License.
-WhatsAsenaDuplicated
+WhatsAsenaPublic - TOXIC DEVIL
 */
 
 const Asena = require('../events');
