03d0277bcad0b274f24202508ac3d8a7d505f8e7
T0X!C D3V!L <jinnherefan2021@gmail.com>
2021-05-07 19:44:17 +0530

    📢 WhatsAsenaPublic Version 1.2 Is Available Now!

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -1,4 +1,12 @@
+/* Copyright (C) 2020 TOXIC DEVIL
 
+CODDED BY TOXIC DEVIL
+
+Licensed under the  GPL-3.0 License;
+
+you may not use this file except in compliance with the License.
+WhatsAsenaPublic - TOXIC DEVIL
+*/
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
